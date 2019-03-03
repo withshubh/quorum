1c08c03895d09b1ab69d446366c4a23ce70f886b
Benjamin Diamond <30356252+benediamond@users.noreply.github.com>
2019-03-03 15:48:19 -0500

    compiles
    At this point just a "rough draft" of what it will ultimately look like.

--- core/zether/api.go
+++ core/zether/api.go
@@ -13,6 +13,7 @@ import (
 	"math/big"
 	"math/rand"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -97,35 +98,36 @@ func (api *PublicZetherAPI) ReadBalance(CLBytes [2]common.Hash, CRBytes [2]commo
 	return 0, errors.New("Balance decryption failed!")
 }
 
-func (api *PublicZetherAPI) ProveTransfer(CLBytes [][2]common.Hash, CRBytes [][2]common.Hash, yBytes [][2]common.Hash, x common.Hash, bTransfer uint64, bDiff uint64, outIndex uint64, inIndex uint64) (map[string]interface{}, error) {
+func (api *PublicZetherAPI) ProveTransfer(CLBytes [][2]common.Hash, CRBytes [][2]common.Hash, yBytes [][2]common.Hash, gEpoch [2]common.Hash, xBytes common.Hash, bTransfer uint64, bDiff uint64, index []uint64) (map[string]interface{}, error) {
 	// note: CL and CR here are before the debits are done, whereas verification takes them after the debits are done.
 	// a bit weird, but makes sense: the contract will have to do them "anyway", whereas geth javascript will not.
+	// edit: actually, CL and CR should represent the state after any "overdue rollovers" are done, but before the debits are done.
 	result := make(map[string]interface{})
 
 	myRand := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	size := yBytes.length
-	CL := make([]string, size)
-	CR := make([]string, size)
-	y := make([]string, size)
+	r, _, err := bn256.RandomG1(myRand)
+	size := len(yBytes)
+	var CL strings.Builder
+	var CR strings.Builder
+	var y strings.Builder
 	for i := 0; i < size; i++ {
-		CL[i] = hexutil.Encode(append(CLBytes[i][0].Bytes(), CLBytes[i][1].Bytes()))
-		CR[i] = hexutil.Encode(append(CRBytes[i][0].Bytes(), CRBytes[i][1].Bytes()))
-		y[i] = hexutil.Encode(append(yBytes[i][0].Bytes(), yBytes[i][1].Bytes()))
+		CL.WriteString(hexutil.Encode(append(CLBytes[i][0].Bytes(), CLBytes[i][1].Bytes()...)))
+		CR.WriteString(hexutil.Encode(append(CRBytes[i][0].Bytes(), CRBytes[i][1].Bytes()...)))
+		y.WriteString(hexutil.Encode(append(yBytes[i][0].Bytes(), yBytes[i][1].Bytes()...)))
 	}
 
 	// RPC to Java service
 	req, _ := http.NewRequest("GET", "http://localhost:8080/prove-transfer", nil)
 	q := req.URL.Query()
-	q.Add("CL", CL)
-	q.Add("CR", CR)
-	q.Add("y", y)
-	q.Add("x", hexutil.Encode(x.Bytes()))
+	q.Add("CL", CL.String())
+	q.Add("CR", CR.String())
+	q.Add("y", y.String())
+	q.Add("gEpoch", hexutil.Encode(append(gEpoch[0].Bytes(), gEpoch[1].Bytes()...)))
+	q.Add("x", hexutil.Encode(xBytes.Bytes()))
 	q.Add("r", common.BytesToHash(r.Bytes()).Hex())
 	q.Add("bTransfer", hexutil.EncodeUint64(bTransfer))
 	q.Add("bDiff", hexutil.EncodeUint64(bDiff))
-	q.add("outIndex", hexutil.EncodeUint64(outIndex))
-	q.add("inIndex", hexutil.EncodeUint64(inIndex))
+	q.Add("index", hexutil.EncodeUint64(index[0])+hexutil.EncodeUint64(index[1]))
 
 	req.URL.RawQuery = q.Encode()
 	client := &http.Client{}
@@ -138,22 +140,22 @@ func (api *PublicZetherAPI) ProveTransfer(CLBytes [][2]common.Hash, CRBytes [][2
 	proof := string(resp_body)
 
 	gBytes, _ := hexutil.Decode("0x077da99d806abd13c9f15ece5398525119d11e11e9836b2ee7d23f6159ad87d401485efa927f2ad41bff567eec88f32fb0a0f706588b4e41a8d587d008b7f875")
-	result["L"] = make([][2]common.Hash, size)
+	L := make([][2]common.Hash, size)
 	for i := 0; i < size; i++ {
-		L := new(bn256.G1)
-		L.Unmarshal(yBytes[i])
-		L.ScalarMult(L, r)
-		if i == outIndex {
+		L_i := new(bn256.G1)
+		L_i.Unmarshal(append(yBytes[i][0].Bytes(), yBytes[i][1].Bytes()...))
+		L_i.ScalarMult(L_i, r)
+		if uint64(i) == index[0] {
 			gOut := new(bn256.G1)
 			gOut.Unmarshal(gBytes)
-			gOut.ScalarMult(big.NewInt(int64(-bTransfer)))
-			L.Add(L, gOut)
-		} else if i == inIndex {
+			gOut.ScalarMult(gOut, big.NewInt(int64(-bTransfer)))
+			L_i.Add(L_i, gOut)
+		} else if uint64(i) == index[1] {
 			gIn := new(bn256.G1)
-			gIn.ScalarMult(big.NewInt(int64(bTransfer)))
-			L.Add(L, gIn)
+			gIn.ScalarMult(gIn, big.NewInt(int64(bTransfer)))
+			L_i.Add(L_i, gIn)
 		}
-		result["L"][i] = [2]common.Hash{common.BytesToHash(L.Marshal()[:32]), common.BytesToHash(L.Marshal()[32:])}
+		L[i] = [2]common.Hash{common.BytesToHash(L_i.Marshal()[:32]), common.BytesToHash(L_i.Marshal()[32:])}
 	}
 	R := new(bn256.G1)
 	R.Unmarshal(gBytes)
@@ -161,13 +163,22 @@ func (api *PublicZetherAPI) ProveTransfer(CLBytes [][2]common.Hash, CRBytes [][2
 	if err != nil {
 		return nil, err
 	}
+	x := new(big.Int)
+	x.SetBytes(xBytes.Bytes())
+	u := new(bn256.G1)
+	u.Unmarshal(append(gEpoch[0].Bytes(), gEpoch[1].Bytes()...))
+	u.ScalarMult(u, x)
+	result["L"] = L
 	result["R"] = [2]common.Hash{common.BytesToHash(R.Marshal()[:32]), common.BytesToHash(R.Marshal()[32:])}
-	result["proof"] = proof // will have to concatenate the shuffle proof!
+	result["u"] = [2]common.Hash{common.BytesToHash(u.Marshal()[:32]), common.BytesToHash(u.Marshal()[32:])}
+	// ^^^ again, all of these extra bits would be unnecessary if elliptic curve ops could be performed directly in javascript.
+	// as another alternative, could add ec operations to the zether namespace, but this is sort of clunky.
+	result["proof"] = proof
 
 	return result, nil
 }
 
-func (api *PublicZetherAPI) ProveBurn(CL [2]common.Hash, CR [2]common.Hash, y [2]common.Hash, bTransfer uint64, x common.Hash, bDiff uint64) (interface{}, error) {
+func (api *PublicZetherAPI) ProveBurn(CL [2]common.Hash, CR [2]common.Hash, y [2]common.Hash, bTransfer uint64, gEpoch [2]common.Hash, x common.Hash, bDiff uint64) (interface{}, error) {
 	bTransferBytes := make([]byte, 32)
 	bDiffBytes := make([]byte, 32)
 	binary.PutUvarint(bTransferBytes, uint64(bTransfer))
@@ -179,6 +190,7 @@ func (api *PublicZetherAPI) ProveBurn(CL [2]common.Hash, CR [2]common.Hash, y [2
 	q := req.URL.Query()
 	q.Add("CL", hexutil.Encode(append(CL[0].Bytes(), CL[1].Bytes()...)))
 	q.Add("CR", hexutil.Encode(append(CR[0].Bytes(), CR[1].Bytes()...)))
+	q.Add("gEpoch", hexutil.Encode(append(gEpoch[0].Bytes(), gEpoch[1].Bytes()...)))
 	q.Add("y", hexutil.Encode(append(y[0].Bytes(), y[1].Bytes()...)))
 	q.Add("x", hexutil.Encode(x.Bytes()))
 	q.Add("bTransfer", hexutil.EncodeUint64(bTransfer))
